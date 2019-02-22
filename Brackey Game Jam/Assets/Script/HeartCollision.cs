c4b8a9f255aa2f792f0dcbc94c1a68a29c6f1363
MadCoderr <msrfarooqi@gmail.com>
2019-02-22 23:49:05 +0500

    Added level 4 and 5 and Counting the Move of player.

--- Brackey Game Jam/Assets/Script/HeartCollision.cs
+++ Brackey Game Jam/Assets/Script/HeartCollision.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class HeartCollision : MonoBehaviour
 {
-    private enum Gender { Male, Female};
-    
+    private enum Gender { Male, Female };
+
     [SerializeField] private GameObject HeartPrefab;
     [SerializeField] private Transform EyePosition;
     [SerializeField] private LayerMask TargetMaks;
@@ -22,7 +22,8 @@ public class HeartCollision : MonoBehaviour
 
     private RaycastHit2D _hit, _hitTop, _hitBottom;
 
-    private void Start() {
+    private void Start()
+    {
         _refOwnParent = GetComponent<Movement>();
         _spriteRender = GetComponentInChildren<SpriteRenderer>();
         var childTransform = transform.GetChild(1).transform;
@@ -32,19 +33,22 @@ public class HeartCollision : MonoBehaviour
         else
             _genderTag = "Female";
 
-        if (_spriteRender.flipX) {
+        if (_spriteRender.flipX)
+        {
             _direction = Vector2.left;
             childTransform.localPosition = new Vector3(0.6f, 0f, 0f);
             var offSet = this.GetComponent<CapsuleCollider2D>().offset;
             this.GetComponent<CapsuleCollider2D>().offset = new Vector2(Mathf.Abs(offSet.x), offSet.y);
         }
-        else {
+        else
+        {
             _direction = Vector2.right;
             childTransform.localPosition = new Vector3(-0.6f, 0f, 0f);
         }
     }
 
-    private void Update() {
+    private void Update()
+    {
         _hit = Physics2D.Raycast(EyePosition.position, _direction, Distance, TargetMaks);
         _hitTop = Physics2D.Raycast(EyePosition.position, Vector2.up, Distance, TargetMaks);
         _hitBottom = Physics2D.Raycast(EyePosition.position, Vector2.down, Distance, TargetMaks);
@@ -53,52 +57,64 @@ public class HeartCollision : MonoBehaviour
         Debug.DrawRay(EyePosition.position, Vector2.up, Color.blue);
         Debug.DrawRay(EyePosition.position, Vector2.down, Color.blue);
 
-        if (_hit.collider != null) {
+        if (_hit.collider != null)
+        {
             _canCollide = true;
-             this.GetComponent<CircleCollider2D>().enabled = false;
-             this.GetComponent<CapsuleCollider2D>().enabled = true;
-        } 
-            
+            this.GetComponent<CircleCollider2D>().enabled = false;
+            this.GetComponent<CapsuleCollider2D>().enabled = true;
+        }
+
     }
 
-    private void checkTopBottomCollision() {
-        if (_hitTop.collider != null) {
+    private void checkTopBottomCollision()
+    {
+        if (_hitTop.collider != null)
+        {
             count++;
             setReference();
             _refOwnParent.MoveToTop = false;
-            _refColliderParent.MoveToBottom = false; 
+            _refColliderParent.MoveToBottom = false;
         }
-        else if (_hitBottom.collider != null) {
-             count++;
-             setReference();
+        else if (_hitBottom.collider != null)
+        {
+            count++;
+            setReference();
             _refOwnParent.MoveToBottom = false;
             _refColliderParent.MoveToTop = false;
-        } else {
+        }
+        else
+        {
             _refOwnParent.MoveToBottom = true;
             _refOwnParent.MoveToTop = true;
 
-            if (_refColliderParent != null) {
+            if (_refColliderParent != null)
+            {
                 _refColliderParent.MoveToTop = true;
                 _refColliderParent.MoveToBottom = true;
             }
         }
+        
     }
 
-    private void setReference() {
-        if (count == 1) {
+    private void setReference()
+    {
+        if (count == 1)
+        {
             if (_hitTop.collider != null) { _refColliderParent = _hitTop.transform.GetComponentInParent<Movement>(); print("ref set"); }
-                
+
             else if (_hitBottom.collider != null) { _refColliderParent = _hitBottom.transform.GetComponentInParent<Movement>(); print("ref set"); }
-                
+
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D other) {
-        if (other.tag == _genderTag && _canCollide && _triggerCount == 1) {
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == _genderTag && _canCollide && _triggerCount == 1)
+        {
             _triggerCount--;
             Instantiate(HeartPrefab, GetComponent<Movement>().getEndPosition(), Quaternion.identity);
             Destroy(other.gameObject, 0.1f);
-            Destroy(this.gameObject, 0.1f); 
+            Destroy(this.gameObject, 0.1f);
         }
     }
 
